f389c3da26cb1b5bdda5ecce1ae6aed3c6117bed
Arshdeep Singh <arshdeepjhagra@gmail.com>
2025-08-25 09:39:57 +0400

    google auth state fix

--- app/Http/Controllers/Web/ProviderCallbackController.php
+++ app/Http/Controllers/Web/ProviderCallbackController.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-use Laravel\Socialite\Facades\Socialite;
+use Socialite;
 
 class ProviderCallbackController extends Controller
 {

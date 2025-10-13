dab7f72f24b49b2f46c9097588bad8a349c7a9a5
Arshdeep Singh <arshdeepjhagra@gmail.com>
2025-10-13 16:12:16 +0400

    checkout fixed

--- resources/views/theme/xtremez/order-confirmation.blade.php
+++ resources/views/theme/xtremez/order-confirmation.blade.php
@@ -1,13 +1,13 @@
 @extends('theme.xtremez.layouts.app')
 
 @section('content')
-  <section class="heading-section py-5">
+  {{-- <section class="heading-section py-5">
     <div class="container">
       <div class="heading-row">
         <h2 class="section-title fs-1 text-center m-0">Order Summary</h2>
       </div>
     </div>
-  </section>
+  </section> --}}
 
   <section class="order-summary-section pb-5">
     <div class="container">
@@ -20,17 +20,17 @@
       </div>
 
       <!-- Main Grid -->
-      <div class="row gx-3 gy-3">
+      <div class="row gx-3 gy-3 bg-body-tertiary">
         <!-- Left Column -->
         <div class="col-md-6">
-          <div class="bg-white p-4 mb-3">
+          <div class=" p-4 mb-3">
             <div class="fw-bold fs-4">Your Order is Confirmed</div>
             <div class="small mt-2 mb-0 fs-6">
               We've accepted your order, and we're getting it ready,<br>
               A confirmation has been sent to <span class="fw-bold">{{ $order->email }}</span>
             </div>
           </div>
-          <div class="bg-white p-4">
+          <div class=" p-4">
             <div class="fw-bold mb-3 fs-4">Customer Details</div>
             <div class="mb-2">
               <span class="fw-bold">Email</span><br>
@@ -59,19 +59,21 @@
 
         <!-- Right Column -->
         <div class="col-md-6">
-          <div class="bg-white p-4 h-100 d-flex flex-column">
+          <div class=" p-4 h-100 d-flex flex-column">
             <!-- Items -->
             <div class="mb-3">
               @foreach ($order->lineItems as $item)
                 <div class="d-flex align-items-center mb-2 order-item">
                   <div class="order-item-img me-2">
-                    <img src="{{ asset('theme/xtremez/assets/images/product.png') }}" alt="Product">
+                    <img src="{{ $item->productVariant->getThumbnail() }}" alt="Product">
                   </div>
                   <div class="flex-grow-1 small fs-6">
-                    {{ $item->productVariant->name ?? 'Product' }}
+                    {{ optional($item->productVariant)->product->translation->name ?? 'Product' }} x {{ $item->quantity }}
+
+
                   </div>
                   <div class="fw-bold ms-2 fs-6 text-nowrap">
-                    {{ number_format($item->price, 2) }} {{ strtoupper(active_currency()) }}
+                    {!! price_format($order->currency->code, ($item->price * $item->quantity )) !!}
                   </div>
                 </div>
               @endforeach
@@ -81,17 +83,19 @@
             <ul class="list-unstyled mt-auto fs-6">
               <li class="d-flex justify-content-between mb-1">
                 <span class="text-muted">Subtotal</span>
-                <span>{{ number_format($order->total, 2) }} {{ strtoupper(active_currency()) }}</span>
+                <span>
+                    {!! price_format($order->currency->code, $order->sub_total) !!}</span>
               </li>
               <li class="d-flex justify-content-between mb-1">
                 <span class="text-muted">Tax</span>
-                <span>0.00 {{ strtoupper(active_currency()) }}</span>
+                <span>
+                    {!! price_format($order->currency->code, $order->tax) !!}</span>
               </li>
               <hr>
               <li class="d-flex justify-content-between pt-2 fw-bold mt-2">
                 <span>Total</span>
-                <span class="text-black">{{ number_format($order->total, 2) }}
-                  {{ strtoupper(active_currency()) }}</span>
+                <span class="text-black">
+                    {!! price_format($order->currency->code, $order->total) !!}</span>
               </li>
             </ul>
           </div>
